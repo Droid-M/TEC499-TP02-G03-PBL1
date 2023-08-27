42e68de10ad8f79ec6a397786c95ef6aabcdebff
Droid-M <62139366+Droid-M@users.noreply.github.com>
2023-08-27 13:45:08 -0300

    move 7 files and update 1 file

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Compilar funções ajudantes
-gcc -c C/helpers/input_helpers.c -o input_helpers.o
-gcc -c C/helpers/output_helpers.c -o output_helpers.o
+gcc -c PC/helpers/input_helpers.c -o input_helpers.o
+gcc -c PC/helpers/output_helpers.c -o output_helpers.o
 
 # Compilar programa principal
-gcc -c C/menu.c -o menu.o
+gcc -c PC/menu.c -o menu.o
 
 # Vincular os arquivos objetos e criar o executavel
 gcc input_helpers.o output_helpers.o  menu.o -o menu
