20b7ce5bf11e6898911c1f420c78d3937c3e0c06
Droid-M <62139366+Droid-M@users.noreply.github.com>
2023-08-28 10:20:01 -0300

    update models.h

--- PC/models/models.h
+++ PC/models/models.h
@@ -12,6 +12,8 @@ struct Sensor {
 
 extern struct Sensor sensors[32];
 
+extern short int in_protocol;
+
 int select_sensor_value(struct Sensor sensor);
 void manage_sensor(unsigned int address, unsigned int command);
 void open_connection();
@@ -19,4 +21,5 @@ void close_connection();
 char *rx_char();
 void tx_char(char *data);
 void tx_hex(unsigned int hex_value);
+int can_start_protocol();
 #endif
